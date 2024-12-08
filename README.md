45d3638fe533a09f41a2fc02e93ba4226f1db65f
Matthew Champagne <champagne7103@gmail.com>
2024-12-07 20:08:19 -0500

    Organized proj so app can use neural network lib

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 For this project we trained a neural network to classify different heart conditions based
 upon the audio captured from a stethescope.
 
+# Prototype Hardware List
+
+1. [Raspberry Pi 4 Model B (4GB)](https://www.amazon.com/Raspberry-Model-2019-Quad-Bluetooth/dp/B07TC2BK1X?crid=2WSB5NCICBTQT&dib=eyJ2IjoiMSJ9.4wZGiZcG7IfVeIs8ylcbr6mlJTaWxTi_gQsqNys_p56g93SxWajOISz7n7NLVuWlO44OOTtn4Di3h0scza847F67H11ZYBsUBKIUb4c-mb89GzgCWAUjjjLXte9O76t7a_IjIZYTVIdJWccl2-LYK9PvkUYUhfaZ1VNsehDPlHaWl5ye0bvkLEeAbWmMLRFfPivZZHmi03oalBw7TCUo_FWONduvms1IvrbXE2b6BeE.g4b0qQn1EIh2ipr_xFNCpH1ccENgtHyV_pvvcBI_d3w&dib_tag=se&keywords=raspberry%2Bpi%2B4%2B4gb&qid=1730238949&sprefix=raspberry%2Bpi%2B4%2B4gb%2Caps%2C63&sr=8-1&th=1)
+2. [TFT Touchscreen LCD Combo](https://www.amazon.com/Elecrow-Display-Monitor-800x480-Raspberry/dp/B013JECYF2?ie=UTF8&linkCode=ll1&tag=mmjjg-20&linkId=df2d8e154ca1d5ca9fb992d34677ebd3)
+3. [Contact Microphone](https://www.amazon.com/TraderPlus-Contact-Microphone-Mandolin-Ukulele/dp/B07795XHLH?dib=eyJ2IjoiMSJ9.-PibDNlinRz79bIpdumGJCj5SKcO5wRmS_PzLIC5PEcEoz0PU5lD3q9dJG217udvm7ZCRAL9eI920l11ieI9KDQl2L_EcE4u42Q6ctQ8B52h8W04AGdgH_Cwmo8HF9gt6I9mP6FaC9_6M3RIbNfx-AIdTX29Lb2_3bWUs85w5Ac0F5UDV-o08cKrLZwze9r-ecVvDuPJxOCJNuDmtSuuRH17aM_4wkZgZ-sKiVa-fBHvIfqOZkhDNFsAFtgX__AuqeWRquZCA3IQylNxUz5xDv5QKWBXUwXb17QRFlXE9dg.wqhUTmOJQvG4WAyFabFC6OWIoQyxcb3-5AYxlKpNW6U&dib_tag=se&keywords=contact+microphone&qid=1730237679&sr=8-6)
+4. [Audio Interface](https://www.amazon.com/Cubilux-Interface-Recording-Podcasting-Microphone/dp/B0CNVYBWX7?dib=eyJ2IjoiMSJ9.Ofrj2bZelm_WzHOYXuF4xD2rZGRMGAFVo9x3D2Boiai_ilZXqte-1ONtO9hSfxS9d2WbchPdunMGFGTGg57dDANOe-8n1zReJjqKjU6mbQET77tCc4HYpP-bu5rUoWFEFwoyDnpWGGYWRI3pbvHwnmtF3bFTqHl8zfzbwXd1lyHoAQPEZFczwQNTQJnCF0dQrsr6dOYcNJJmpA1ZjsGf7u5hgeLtRfpolzLeWaRFPshqB_uLMAsB69wq-vmQFyKhQPn8YxvrmY9a5ScIeNkR4bmDTDaBEQLwWc8pmgD3kcM.LMWHNc9LS0k0V2QQgOzuBAtqeipPFGg7KTkkakTJcyM&dib_tag=se&keywords=1%2F4+inch+mic+to+usb+recording&qid=1730239633&sr=8-3)
+
 # Presentations
 
 1. Project Proposal -> [Google Slides](https://docs.google.com/presentation/d/1aXbahbYvR3ogfZQphlLMT6WEJyvsSsBNSmaAJ0Ditho/edit?usp=sharing)
---
+++ train.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+
+from neural_network import *
+
+TEST_AMOUNT = 0.2
+BATCH_SIZE = 30
+DIV_FAC = 10
+EPOCHS = 1000
+STEPS = EPOCHS // DIV_FAC
+
+IMG_DIR = ".out_files"
+
+
+def main():
+    if not os.path.exists(IMG_DIR):
+        os.mkdir(IMG_DIR)
+
+    print("Preprocessing Audio data")
+    directories = [os.path.join("dataset", "Audio Files")]
+    x_data, y_data = preprocess(directories)
+
+    print("Loading Audio Data")
+    train_dl, test_dl = get_datasets(
+        x_data, y_data, TEST_AMOUNT, BATCH_SIZE, CustomDataset
+    )
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = DiagnosisNetwork().to(device)
+    lossfn = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters())
+
+    model_summary(model, optim)
+
+    loss_vals = []
+    accuracy_hist = []
+
+    top_1 = evaluate(device, test_dl, model)
+    accuracy_hist.append(top_1)
+    for i in range(DIV_FAC):
+        loss = train(
+            device,
+            train_dl,
+            optim,
+            lossfn,
+            model,
+            STEPS,
+            STEPS * i,
+        )
+        top_1 = evaluate(device, test_dl, model)
+
+        loss_vals.extend(loss)
+        accuracy_hist.append(top_1)
+
+    accuracy_img = os.path.join(IMG_DIR, "accuracy.png")
+    plot_accuracy(accuracy_hist, accuracy_img)
+
+    loss_img = os.path.join(IMG_DIR, "loss.png")
+    plot_loss(loss_vals, loss_img)
+
+    weights_path = os.path.join(os.getcwd(), "model_weights.pt")
+    torch.save(model.state_dict(), weights_path)
+
+
+def model_summary(model, optim):
+    print("Model's state_dict:")
+    for param_tensor in model.state_dict():
+        print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+    print("Optimizer's state_dict:")
+    for var_name in optim.state_dict():
+        print(var_name, "\t", optim.state_dict()[var_name])
+
+
+def plot_accuracy(accuracy, output_path):
+    epochs = list(map(lambda x: x * STEPS, [x for x in range(len(accuracy))]))
+
+    plt.plot(
+        epochs,
+        accuracy,
+        linestyle="-",
+        color="b",
+        label="Accuracy",
+    )
+
+    plt.title("Model Top 1 Accuracy Over Epochs")
+    plt.xlabel("Epochs")
+    plt.ylabel("Top 1 Accuracy")
+
+    plt.ylim(0, 100)
+
+    plt.grid()
+    plt.legend()
+    plt.savefig(output_path, dpi=300)
+    plt.close()
+
+
+def plot_loss(loss_vals, output_path):
+    epochs = [x for x in range(len(loss_vals))]
+
+    plt.plot(
+        epochs,
+        loss_vals,
+        linestyle="-",
+        color="b",
+        label="Loss",
+    )
+
+    plt.title("Model Loss Over Epochs")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss Smoothed")
+
+    plt.ylim(0, max(loss_vals))
+
+    plt.grid()
+    plt.legend()
+    plt.savefig(output_path, dpi=300)
+    plt.close()
+
+
+def get_data_sample(train_dl):
+    [x, y] = next(iter(train_dl))
+    input_2d_array, input_scalar = x
+    input_scalar = input_scalar.unsqueeze(1).float()
+
+    return input_2d_array, input_scalar
+
+
+if __name__ == "__main__":
+    main()
